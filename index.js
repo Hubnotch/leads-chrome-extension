5a436f81bf2720ac629dab0f6ed4de546733aaa9
hubnotch <love2ekene@gmail.com>
2022-02-05 02:34:50 +0100

    added manifest.js file

--- index.js
+++ index.js
@@ -8,13 +8,26 @@ const ulEl = document.getElementById("ul-el");
 inputBtn.addEventListener("click", function (){
    let inputValue = inputEl.value;
    myLeads.push(inputValue);
+   inputEl.value = "";
     renderLeads()
 });
 
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>";
+    // listItems +=
+    //   "<li><a target='_blank' href='" +
+    //   myLeads[i] +
+    //   "'>" +
+    //   myLeads[i] +
+    //   "</a></li>";
+      listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `;
   }
   ulEl.innerHTML = listItems;
 }
