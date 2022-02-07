32fadb2ce6129fbe7cf61349debf83942450dc09
hubnotch <love2ekene@gmail.com>
2022-02-07 15:30:18 +0100

    refactored code to be dynamic

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
-
 let myLeads = [];
-
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-inputBtn.addEventListener("click", function (){
-   let inputValue = inputEl.value;
-   myLeads.push(inputValue);
-   inputEl.value = "";
-    renderLeads()
-});
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
 
-function renderLeads() {
+
+function render(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
-    // listItems +=
-    //   "<li><a target='_blank' href='" +
-    //   myLeads[i] +
-    //   "'>" +
-    //   myLeads[i] +
-    //   "</a></li>";
-      listItems += `
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `;
@@ -32,4 +25,15 @@ function renderLeads() {
   ulEl.innerHTML = listItems;
 }
 
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
 
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
